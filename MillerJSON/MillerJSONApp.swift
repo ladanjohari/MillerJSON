a1a511738cf1e869e8145fc5559be44c672726f5
Ladan Johari <laadanjohari@gmail.com>
2024-11-18 21:47:07 -0800

    Initial Commit

---
+++ MillerJSON/MillerJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillerJSONApp.swift
+//  MillerJSON
+//
+//  Created by Ladan Johari on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MillerJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
